6119a56175e0279e6754b42ed15574f6c5a8643b
Abir-Tx <28858998+Abir-Tx@users.noreply.github>
2020-09-25 15:34:55 +0600

    Counter input page add

---
+++ app/src/main/java/com/example/xwriter/Counter.java
@@ -0,0 +1,14 @@
+package com.example.xwriter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Counter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_counter);
+    }
+}
